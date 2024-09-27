cef86d8d14e6082cdcba9f6549b216d28db890a6
Guy Wilson <guywilsonjr@gmail.com>
2024-09-26 17:11:45 -0700

    interim

--- build_python.sh
+++ build_python.sh
@@ -1,4 +1,3 @@
-
 set -u
 set -e
 #bad eval source nostacklimit flto
@@ -7,13 +6,14 @@ set -e
 #export CPPFLAGS='-Wno-ignored-optimization-argument -Wno-profile-instr-unprofiled -Wno-parentheses-equality -Wno-unused-value -Wno-empty-body -Qunused-arguments -flto -finstrument-functions -fno-sanitize=all -fno-strict-float-cast-overflow -fno-strict-overflow -Ofast -fmerge-all-constants -fdenormal-fp-math=positive-zero  -fstrict-aliasing -fomit-frame-pointer -fwhole-program-vtables -march=native'
 
 #export CPPFLAGS='-Wno-ignored-optimization-argument -Wno-profile-instr-unprofiled -Wno-parentheses-equality -Wno-unused-value -Wno-empty-body -Qunused-arguments -finstrument-functions -fno-sanitize=all -fno-strict-overflow'
+#good profiling opts?
+# -fprofile-arcs -fbranch-probabilities
 export PREFIX=$1
 export CCACHE_COMPILERTYPE=clang
 export CC='/usr/lib/llvm-18/bin/clang'
 export CXX=$CC
-#export BEST_OPT_FLAGS='-Ofast -march=native'
-export BEST_OPT_FLAGS=''
-export CFLAGS=''
+export BEST_OPT_FLAGS='-Ofast -march=native'
+export CFLAGS='-fno-reorder-blocks-and-partition'
 export CXXFLAGS=""
 export CPPFLAGS=$CXXFLAGS
 export BASECPPFLAGS=""
@@ -35,17 +35,17 @@ export PY_STDMODULE_CFLAGS="$PY_CFLAGS $PY_CFLAGS_NODIST $PY_CPPFLAGS $CFLAGSFOR
 # LIBEXPAT_CFLAGS
 # LIBHACL_CFLAGS
 # PY_CORE_CFLAGS
-# LIBMPDEC_CFLAGS
-# LIBMPDEC_CFLAGS
-# LIBMPDEC_CFLAGS
-# LIBMPDEC_CFLAGS
 export PY_BUILTIN_MODULE_CFLAGS="$BEST_OPT_FLAGS $PY_STDMODULE_CFLAGS -DPy_BUILD_CORE_BUILTIN"
 export LIBMPDEC_CFLAGS=''
 export BUILD_CORES=8
-#export COMPILEALL_OPTS="-j $BUILD_CORES -o 0 -o 1 -o 2"
+export COMPILEALL_OPTS="-j $BUILD_CORES -o 0 -o 1 -o 2"
+#potentialprofiletaskargs
+# '-ucurses,largfile,network,decimal,cpu,subprocess,urlfetch,gui,tzdata'
+# Maybenot -uwalltime,largefile, -M for memory intensive >2.5Gb
+export PROFILE_TASK="-m test -o --pgo-extended --timeout=600"
 
 echo "configuring"
-./conf_python.sh $PREFIX
+./conf_python.sh "$PREFIX"
 echo "configured"
 make -j $BUILD_CORES
 echo "made"
--- conf_python.sh
+++ conf_python.sh
@@ -1,15 +1,15 @@
 set -u
 set -e
-export PREFIX=$1
 #make distclean && rm -rf ./default.profraw autom4te.cache/ ./gmon.out
 
 export CCACHE_COMPILERTYPE=clang
 export CC='clang'
 export CXX=$CC
-#export PROFILE_TASK='-m test -o -j 2 --pgo --timeout=600'
-
-
-export confopts="--prefix=$PREFIX" # --enable-loadable-sqlite-extensions --without-decimal-contextvar --with-dbmliborder=gdbm:ndbm:bdb --with-pkg-config=yes --with-ensurepip=upgrade --enable-profiling --with-computed-gotos --without-doc-strings --with-lto=full --enable-optimizations --enable-ipv6 --enable-bolt"
+#proftask -r for random
+# Release configure flags
+# --disable-test-modules
+#
+export confopts="--prefix=$PREFIX --enable-loadable-sqlite-extensions --without-decimal-contextvar --with-dbmliborder=gdbm:ndbm:bdb --with-pkg-config=yes --with-ensurepip=upgrade --enable-profiling --with-computed-gotos --without-doc-strings --with-lto=full --enable-optimizations --enable-ipv6 --enable-bolt"
 #NOTE: BOLT is currently incompatible with the -freorder-blocks-and-partition compiler option. Since GCC8 enables this option by default, you have to explicitly disable it by adding -fno-reorder-blocks-and-partition flag if you are compiling with GCC8 or above.
 ./configure $confopts
 
